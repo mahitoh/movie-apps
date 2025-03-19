935f71e61ec9afb1dedb4690de6e3916b7b8e835
mahitoh <richcalchia@gmail.com>
2025-03-19 20:36:18 +0100

    first commit of movie app with react native

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+const search = () => {
+  return (
+    <View>
+      <Text>search</Text>
+    </View>
+  )
+}
+export default search
+const styles = StyleSheet.create({})
---
+++ app/movie/[id].tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>MovieDetails</Text>
+    </View>
+  );
+};
+export default MovieDetails;
+const styles = StyleSheet.create({});
